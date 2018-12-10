33a6cd775a1e7d4334d61eab27ee404d87716552
Markus Kivikoski <a170456@myy.haaga-helia.fi>
2018-12-10 08:10:01 +0200

    Rest api lisätty

--- src/main/java/fi/haagahelia/course/Ohjelmistoprojekti/domain/VastausRepository.java
+++ src/main/java/fi/haagahelia/course/Ohjelmistoprojekti/domain/VastausRepository.java
@@ -3,7 +3,8 @@ package fi.haagahelia.course.Ohjelmistoprojekti.domain;
 import java.util.Optional;
 
 import org.springframework.data.repository.CrudRepository;
-
+import org.springframework.stereotype.Repository;
+@Repository
 public interface VastausRepository extends CrudRepository<Vastaus, Long> {
 	
 	Optional<Vastaus> findById(Long id);

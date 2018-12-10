56867df255db6ce18a6c6a24eac55f5ddedd6122
AnniL <AnniLaakko@noreply.github.com>
2018-12-10 11:10:01 +0200

    Vastattu edittiä vaikka sitä nyt ei sitten tarvitakkaan
    Kiitti kaverit kun saan tehdä niin paljon.

--- src/main/java/fi/haagahelia/course/Ohjelmistoprojekti/domain/Vastattu.java
+++ src/main/java/fi/haagahelia/course/Ohjelmistoprojekti/domain/Vastattu.java
@@ -1,13 +1,12 @@
 /*package fi.haagahelia.course.Ohjelmistoprojekti.domain;
 
-import java.util.List;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.OneToMany;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 
 @Entity
 public class Vastattu {
@@ -16,35 +15,50 @@ public class Vastattu {
 	@GeneratedValue(strategy=GenerationType.AUTO)
 	private Long vastattuId;
 	
-	@OneToMany(cascade = CascadeType.ALL, mappedBy = "vastaus")
-	private List<Vastaus> vastaukset;
+	@ManyToOne
+	@JoinColumn(name = "vastausid")
+	private Vastaus vastaus;
 	
-	@OneToMany(cascade = CascadeType.ALL, mappedBy = "vastausvaihtoehto")
-	private List<VastausVaihtoehto> vastausvaihtoehdot;
-
-	public List<Vastaus> getVastaukset() {
-		return vastaukset;
+	@ManyToOne
+	@JoinColumn(name = "vastausvaihtoehtoid")
+	private VastausVaihtoehto vastausvaihtoehto;
+	
+	public Vastattu(Long vastattuId, Vastaus vastaus, VastausVaihtoehto vastausvaihtoehto){
+		super();
+		this.vastattuId = vastattuId;
+		this.vastaus = vastaus;
+		this.vastausvaihtoehto = vastausvaihtoehto;
 	}
 
-	public void setVastaukset(List<Vastaus> vastaukset) {
-		this.vastaukset = vastaukset;
+
+	public Vastaus getVastaus() {
+		return vastaus;
 	}
 
-	public List<VastausVaihtoehto> getVastausvaihtoehdot() {
-		return vastausvaihtoehdot;
+
+	public void setVastaus(Vastaus vastaus) {
+		this.vastaus = vastaus;
 	}
 
-	public void setVastausvaihtoehdot(List<VastausVaihtoehto> vastausvaihtoehdot) {
-		this.vastausvaihtoehdot = vastausvaihtoehdot;
+
+	public VastausVaihtoehto getVastausvaihtoehto() {
+		return vastausvaihtoehto;
 	}
+
+
+	public void setVastausvaihtoehto(VastausVaihtoehto vastausvaihtoehto) {
+		this.vastausvaihtoehto = vastausvaihtoehto;
+	}
+
 
 	@Override
 	public String toString() {
-		return "Vastattu [vastattuId=" + vastattuId + ", vastaukset=" + vastaukset + ", vastausvaihtoehdot="
-				+ vastausvaihtoehdot + "]";
+		return "Vastattu [vastattuId=" + vastattuId + ", vastaus=" + vastaus + ", vastausvaihtoehto="
+				+ vastausvaihtoehto + "]";
 	}
+
 	
 	
 	
-}
-*/
+	
+}*/

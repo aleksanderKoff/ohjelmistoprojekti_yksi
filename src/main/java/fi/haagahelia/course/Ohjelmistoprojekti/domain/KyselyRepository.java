5d89b7c0eb3c37c428bfb7b93d772cfe39f08507
AnniLaakko <39989621+AnniLaakko@users.noreply.github.com>
2018-12-12 17:20:56 +0200

    Vaihdettu lista kommentiksi

--- src/main/java/fi/haagahelia/course/Ohjelmistoprojekti/domain/KyselyRepository.java
+++ src/main/java/fi/haagahelia/course/Ohjelmistoprojekti/domain/KyselyRepository.java
@@ -8,5 +8,5 @@ import org.springframework.data.repository.CrudRepository;
 public interface KyselyRepository extends CrudRepository<Kysely, Long> {
 	
 	Optional<Kysely> findById(Long id);
-	List<Kysely> findByOtsikko(String Otsikko);
+	//List<Kysely> findByOtsikko(String Otsikko);
 }
